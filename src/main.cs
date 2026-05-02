85912c72e373db87bfef8e2ffcb7a9b5ae961eed
Its0mar <omarjadallah24@gmail.com>
2026-05-02 06:31:19 +0300

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -3,5 +3,8 @@ class Program
     static void Main()
     {
         Console.Write("$ ");
+
+        var command = Console.ReadLine();
+        Console.WriteLine($"{command}: command not found");
     }
 }
